38991cb8a47f6033fb4cdea5ae359782df368b58
yeard-morshed <yeardmorshed1999@gmail.com>
2022-08-09 22:38:33 +0600

    userController.2

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class userController extends Controller
 {
@@ -12,10 +14,28 @@ class userController extends Controller
     }
 
 
-    public function load_userDashboard()
+    public function load_userDashboard(Request $req)
     {
-        return view('userDashboard');
-    }
+//......................................
+        $result2 = $result = DB::table('posts')->where('status', '!=', 'archived')->where('urgent', false)->get();
+        // dd($result2);
+        // $result2 = $result = DB::table('posts')->where('status', '!=', 'archived')->where('urgent', false)->take(3)->get();
+       
+        $list = array();
+       
+       
 
+       
+        // right search results;
+        $result = DB::table('posts')->where('status', '!=', 'archived')->where('urgent', true)->get();
+        //$result = DB::table('posts')->where('status', '!=', 'archived')->where('urgent', true)->take(3)->get();
+
+        // dd($result['rightResults']);
+        $sendData['leftResults'] = $result2;
+        $sendData['rightResults'] = $result;   
+
+        return view('userDashboard',$sendData);
+    }
+//....................................................
 
 }
