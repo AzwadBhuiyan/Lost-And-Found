f0301be26f76ff92ed56c6386e1ae345e0183bae
UmmeHaniJuhi <ummehanijuhi@gmail.com>
2022-08-12 12:16:09 +0600

    con

--- resources/views/footer/contactUs.blade.php
+++ resources/views/footer/contactUs.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
-<html>
-<head>
-<!--Boostrap 5 CDN linl --> <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!--Custom file link -->
- <!--<link rel="stylesheet" href="styleee.css"> -->
- <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Portfolio</title>    
 
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
-</head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+            <link rel="stylesheet" href="css/contact.css">
+            <link rel="stylesheet" href="css/responsive.css">
+            
+        
+    </head>
+
+    
 <body  data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="75">
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
