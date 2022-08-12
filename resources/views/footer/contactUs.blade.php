28852f8e3455311df5aced52e55bd2edc03897ac
UmmeHaniJuhi <ummehanijuhi@gmail.com>
2022-08-12 12:22:01 +0600

    wrf

--- resources/views/footer/contactUs.blade.php
+++ resources/views/footer/contactUs.blade.php
@@ -12,8 +12,9 @@
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-            <link rel="stylesheet" href="css/contact.css">
-            <link rel="stylesheet" href="css/responsive.css">
+            <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
             
         
     </head>
