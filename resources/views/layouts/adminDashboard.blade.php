2bddf00393c6bbd653e044d3b6db72e5eeb6497f
Islam Bin Mursalin <90448168+Mursalin1011@users.noreply.github.com>
2022-08-23 17:29:08 +0600

    button on admindashboard layout

--- resources/views/layouts/adminDashboard.blade.php
+++ resources/views/layouts/adminDashboard.blade.php
@@ -23,7 +23,7 @@
                                 <div class="card bg-primary text-white mb-4">
                                     <div class="card-body">Posts Dashboard</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Posts</a>
+                                        <a class="small text-white stretched-link" href="/admin_dashboard">View Posts</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
