4d6c45ecaaa3dfb5cee1e5eecb437e414a154ef0
Islam Bin Mursalin <90448168+Mursalin1011@users.noreply.github.com>
2022-08-24 00:40:13 +0600

    minor updates

--- resources/views/adminDashboard/posts.blade.php
+++ resources/views/adminDashboard/posts.blade.php
@@ -24,7 +24,7 @@
                     {{-- <th>Email</th> --}}
                     <th></th>
                     <th></th>
-                    <th></th>
+                    <th>Actions</th>
                     <th></th>
 
                 </tr>
